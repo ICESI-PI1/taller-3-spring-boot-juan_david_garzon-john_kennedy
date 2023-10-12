8cf10a16972fedae23f6d6fcef48dad37e86da6f
JDGarzon <93989744+JDGarzon@users.noreply.github.com>
2023-10-11 19:55:25 -0500

    Complete JWT implementation

--- taller3/src/main/java/com/edu/icesi/taller3/security/WebSecurityConfig.java
+++ taller3/src/main/java/com/edu/icesi/taller3/security/WebSecurityConfig.java
@@ -9,17 +9,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.AllArgsConstructor;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@AllArgsConstructor
 public class WebSecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http,AuthenticationManager manager) throws Exception {
+
+        JWTAuthenticationFilter jwtAuthenticationFilter = new JWTAuthenticationFilter();
+        jwtAuthenticationFilter.setAuthenticationManager(manager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests(requests -> requests
@@ -28,10 +37,12 @@ public class WebSecurityConfig {
                 .httpBasic(withDefaults())
                 .sessionManagement(management -> management
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
-    @Bean
+  /*  @Bean
     UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(
@@ -41,12 +52,12 @@ public class WebSecurityConfig {
                     .build()
                 );
         return manager;
-    }
+    }*/ 
 
     @Bean    
     AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
-        .userDetailsService(userDetailsService())
+        .userDetailsService(userDetailsService)
         .passwordEncoder(passwordEncoder())
         .and()
         .build();
@@ -55,5 +66,9 @@ public class WebSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("admin"));
     }
 }
